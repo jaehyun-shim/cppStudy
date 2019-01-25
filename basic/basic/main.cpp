cab2eacc7f36515326fcabf413c92294a2424945
shim <jhshim3478@gmail.com>
2019-01-25 16:02:55 +0900

    Cpp First commit

---
+++ basic/basic/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  basic
+//
+//  Created by shim on 2019. 1. 25..
+//  Copyright © 2019년 shim. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
